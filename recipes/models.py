c9b6e2bf4280c1e871d3a71466200a415883f783
AnnaNovikova <novikova-a98@mail.ru>
2018-04-07 14:31:35 +0300

    = patient_initials added to recipe, __str__ methods added to models

--- recipes/models.py
+++ recipes/models.py
@@ -37,16 +37,23 @@ class User(AbstractUser):
 class Doctor(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
 
+    def __str__(self):
+        return self.user.email
+
 
 class Recipe(models.Model):
     doctor = models.ForeignKey(Doctor, on_delete=models.CASCADE)
     patient_email = models.CharField(max_length=50)
+    patient_initials = models.CharField(max_length=100)
     date = models.DateTimeField()
     token = models.TextField()
-    day_duration = models.SmallIntegerField(default=-1)
+    day_duration = models.PositiveIntegerField(default=15)
     patient_age = models.PositiveSmallIntegerField()
     medicine_card_number = models.CharField(max_length=10, null=True)
     medicine_policy_number = models.CharField(max_length=16, null=True)
+
+    def __str__(self):
+        return '{} - {} - {}'.format(self.date, self.patient_email, self.doctor.user.email)
 
 
 class Pharmacy(models.Model):
@@ -67,6 +74,9 @@ class MedicineName(models.Model):
     medicine_name = models.CharField(max_length=50)
     medicine_description = models.TextField()
 
+    def __str__(self):
+        return self.medicine_name
+
 
 class Medicine(models.Model):
     medicine_name = models.ForeignKey(MedicineName, on_delete=models.CASCADE)
@@ -74,12 +84,18 @@ class Medicine(models.Model):
     medicine_level = models.PositiveSmallIntegerField(default=0)
     pharmacies = models.ManyToManyField(Pharmacy, through='recipes.MedicinesPharmacies')
 
+    def __str__(self):
+        return '{} {}'.format(self.medicine_name.medicine_name, self.medicine_type.type_name)
+
 
 class MedicineDosage(models.Model):
     dosage = models.TextField()
     frequency = models.TextField()
     period = models.TextField()
     medicine = models.ForeignKey(Medicine, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return '{} {} {}'.format(self.medicine.medicine_name.medicine_name, self.dosage, self.frequency)
 
 
 class MedicineRequest(models.Model):
