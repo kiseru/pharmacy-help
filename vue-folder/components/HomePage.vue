3e670a1227397b146fb479ae402071dc31f7d3db
Alexandr Kiselev <kis.alex97@gmail.com>
2018-04-08 00:26:03 +0300

    Added Login form

---
+++ vue-folder/components/HomePage.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <home-page-form/>
+  </div>
+</template>
+
+<script>
+  import HomePageForm from './forms/HomePageForm';
+
+  export default {
+    name: "HomePage",
+    components: {
+      HomePageForm
+    },
+    data() {
+      return {
+        isLoginForm: true
+      }
+    }
+  }
+</script>
+
+<style lang="less" scoped>
+
+</style>
---
+++ vue-folder/components/forms/HomePageForm.vue
@@ -0,0 +1,60 @@
+<template>
+  <div class="form">
+    <p>Войти</p>
+    <form action="/" method="post">
+      <div>
+        <label for="email">E-mail</label>
+        <br>
+        <input type="text" id="email" name="email">
+      </div>
+      <div>
+        <label for="password">Пароль</label>
+        <br>
+        <input type="text" id="password" name="password">
+      </div>
+      <button type="submit">Войти</button>
+    </form>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "HomePageForm"
+  }
+</script>
+
+<style lang="less" scoped>
+  .form {
+    margin: 150px auto;
+    text-align: center;
+    border: 2px solid #087e8b;
+    border-radius: 10px;
+    width: 300px;
+    height: 260px;
+    background-color: #ffffff;
+
+    p {
+      font-size: 24px;
+      margin-top: 20px;
+      margin-bottom: 20px;
+    }
+
+    button {
+      background-color: #087e8b;
+      border: 2px solid #087e8b;
+      border-radius: 5px;
+      color: #ffffff;
+      margin-top: 20px;
+      font-size: 18px;
+      width: 100px;
+      height: 36px;
+      outline: none;
+      transition: ease .8s;
+
+      &:hover {
+        background-color: #ffffff;
+        color: #087e8b;
+      }
+    }
+  }
+</style>
