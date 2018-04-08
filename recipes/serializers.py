031f7c04e33a4dc39685d156796b7665262aef39
AnnaNovikova <novikova-a98@mail.ru>
2018-04-08 15:03:19 +0300

    + full recipe serializer

--- recipes/serializers.py
+++ recipes/serializers.py
@@ -27,3 +27,35 @@ class RecipeSerializerShort(JsonSerializer):
         recipe['patient_email'] = object.patient_email
         recipe['date'] = object.date.strftime('%Y-%m-%d %H:%M')
         return recipe
+
+
+class MedicineRequestSerializer(JsonSerializer):
+    def get_json(self, object: MedicineRequest):
+        result = dict()
+        result['is_accepted'] = object.medicine_count > 0
+        result['medicine_name'] = object.medicine_dosage.medicine.medicine_name
+        result['medicine_frequency'] = object.medicine_dosage.frequency
+        result['medicine_dosage'] = object.medicine_dosage.dosage
+        result['medicine_period'] = object.medicine_dosage.period
+        return result
+
+
+class RecipeSerializerFull(JsonSerializer):
+    medicineRequestSerializer = MedicineRequestSerializer()
+
+    def get_json(self, object: Recipe):
+        recipe = dict()
+        recipe['id'] = object.id
+        recipe['doctor_initials'] = object.doctor.user.last_name + ' ' + object.doctor.user.first_name
+        recipe['doctor_email'] = object.doctor.user.email
+        recipe['patient_initials'] = object.patient_initials
+        recipe['patient_email'] = object.patient_email
+        recipe['date'] = object.date.strftime('%Y-%m-%d %H:%M')
+        recipe['day_duration'] = object.day_duration
+        recipe['patient_age'] = object.patient_age
+        recipe['medicine_card_number'] = object.medicine_card_number
+        recipe['medicine_policy_number'] = object.medicine_policy_number
+        medicineRequests = object.medicinerequest_set.all()
+        requests = [self.medicineRequestSerializer.get_json(i) for i in medicineRequests]
+        recipe['requests'] = requests
+        return recipe
