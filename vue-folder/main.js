97313cfa20ef8d48181e3f6bfa8acb87add2d2ac
KiselevAlexandr <kis.alex97@gmail.com>
2018-04-09 15:39:02 +0300

    Added VueCookies package

--- vue-folder/main.js
+++ vue-folder/main.js
@@ -1,10 +1,12 @@
 import Vue from "vue";
+import VueCookies from "vue-cookies";
 import VueRouter from "vue-router";
 import routes from "./routes";
 import App from "./App.vue";
 
 Vue.config.productionTip = false;
 
+Vue.use(VueCookies);
 Vue.use(VueRouter);
 
 const router = new VueRouter({
