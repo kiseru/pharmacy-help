7ea8c4bbe88fc731357a8d25e88f16362fbfa84e
AnnaNovikova <novikova-a98@mail.ru>
2018-04-10 14:39:40 +0300

    ! redirect in login

--- recipes/urls.py
+++ recipes/urls.py
@@ -4,16 +4,16 @@ from django.views.generic import TemplateView
 from recipes.views import *
 
 urlpatterns = [
-    url('', do_login, name='login'),
-    url('logout/', do_logout, name='logout'),
-    url('api/user/', user_info, name='user_info'),
-    url('api/test_user_info/', test_user_info, name='test_user_info'),
-    url('api/recipes/', RecipesListJsonView.as_view(), name='recipes'),
-    url('medicine/', TemplateView.as_view(template_name='recipes/index.html')),
-    url('medicine/add/', TemplateView.as_view(template_name='recipes/index.html')),
-    url('recipes/<str:token>', TemplateView.as_view(template_name='recipes/index.html')),
-    url('recipes/', TemplateView.as_view(template_name='recipes/index.html')),
-    url('apothecary/', TemplateView.as_view(template_name='recipes/index.html')),
-    url('doctor/', TemplateView.as_view(template_name='recipes/index.html')),
+    url('^$', do_login, name='login'),
+    url('^logout/', do_logout, name='logout'),
+    url('^api/user/', user_info, name='user_info'),
+    url('^api/test_user_info/', test_user_info, name='test_user_info'),
+    url('^api/recipes/', RecipesListJsonView.as_view(), name='recipes'),
+    url('^medicine/', TemplateView.as_view(template_name='index.html')),
+    url('^medicine/add/', TemplateView.as_view(template_name='index.html')),
+    url('^recipes/<str:token>', TemplateView.as_view(template_name='index.html')),
+    url('^recipes/', TemplateView.as_view(template_name='index.html')),
+    url('^apothecary/', TemplateView.as_view(template_name='index.html')),
+    url('^doctor/', TemplateView.as_view(template_name='index.html')),
 ]
 
