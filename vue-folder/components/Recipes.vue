8a25327437d5d0700d36c31c807abde6af7f95df
KiselevAlexandr <kis.alex97@gmail.com>
2018-04-13 17:23:42 +0300

    Added page with recipes for apothecaries

---
+++ vue-folder/components/Recipes.vue
@@ -0,0 +1,80 @@
+<template>
+  <div>
+    <apothecary-header/>
+    <div id="search-input">
+      <app-input id="search" label="Найти"/>
+    </div>
+    <div id="recipes">
+      <div v-for="recipe in recipes">
+        <recipe-card v-bind:recipe="recipe"/>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+  import axios from "axios";
+
+  import ApothecaryHeader from "./header/ApothecaryHeader";
+  import AppInput from "./partials/AppInput";
+  import RecipeCard from "./partials/RecipeCard";
+
+  export default {
+    name: "Recipes",
+    components: {
+      RecipeCard,
+      AppInput,
+      ApothecaryHeader
+    },
+    data() {
+      return {
+        recipes: [
+          {
+            id: "906635418151ba60480cd745256310229175",
+            patientName: "asdfas",
+            doctorName: "asdfasdf",
+            date: "28.07.2018"
+          },
+          {
+            id: "906635418151ba60480cd745256310229175",
+            patientName: "asdfas",
+            doctorName: "asdfasdf",
+            date: "28.07.2018"
+          }
+        ]
+      }
+    },
+    /*beforeMount() {
+      axios.get("/api/recipes")
+        .then(response => this.recipes = response.data)
+        .catch(error => this.recipes = [])
+    }*/
+  }
+</script>
+
+<style lang="less" scoped>
+  #search-input {
+    margin: 10px auto;
+    text-align: center;
+    width: 300px;
+  }
+
+  #recipes {
+    display: grid;
+    margin: 10px auto;
+  }
+
+  @media screen and (min-width: 1080px) {
+    #recipes {
+      grid-template-columns: 540px 540px;
+      width: 1080px;
+    }
+  }
+
+  @media screen and (max-width: 1079px) {
+    #recipes {
+      grid-template-columns: 540px;
+      width: 540px;
+    }
+  }
+</style>
---
+++ vue-folder/components/partials/RecipeCard.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="recipe">
+    <div class="id">ID: {{ recipe.id }}</div>
+    <div class="recipe-man-name">Пациент: {{ recipe.patientName }}</div>
+    <div class="recipe-man-name">Доктор: {{ recipe.doctorName }}</div>
+    <div class="recipe-date">{{ recipe.date }}</div>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "RecipeCard",
+    props: {
+      recipe: {
+        type: Object,
+        required: true
+      }
+    }
+  }
+</script>
+
+<style lang="less" scoped>
+  .recipe {
+    border: 2px solid #087e8b;
+    margin: 20px;
+    height: 205px;
+    background-color: #ffffff;
+    border-radius: 10px;
+    min-width: 500px;
+    padding: 20px;
+    font-size: 22px;
+    text-align: right;
+
+    .id {
+      font-size: 20px;
+      margin-bottom: 20px;
+    }
+
+    .recipe-date {
+      margin-top: 30px;
+      font-size: 18px;
+      text-align: left;
+    }
+  }
+</style>
