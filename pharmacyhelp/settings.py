75cca079f3bbc82ac00ea00367cae314804819dc
Alexandr Kiselev <kis.alex97@gmail.com>
2019-08-19 16:29:15 +0300

    Change database to SQLite3

--- pharmacyhelp/settings.py
+++ pharmacyhelp/settings.py
@@ -82,12 +82,8 @@ WSGI_APPLICATION = 'pharmacyhelp.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'pharmacy',
-        'USER': 'pharmacy_admin',
-        'PASSWORD': 'pharmkety',
-        'HOST': '',
-        'PORT': 5432
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
